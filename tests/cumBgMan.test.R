96ecf325d1353f3c9ed43ff77d0732443f868bc5
Nanna Løjborg <nannalojborg@hotmail.com>
2019-08-23 19:52:08 +0200

    Test new split cumBg functions

---
+++ tests/cumBgMan.test.R
@@ -0,0 +1,65 @@
+# For testing the biogas functions while writing help files
+# S. Hafner
+
+# Load data and functions
+# setwd() 
+
+#files <- list.files('biogas/R', full.names = TRUE)
+#for(i in files) source(i)
+
+#files <- list.files('biogas/data', full.names = TRUE)
+#for(j in files) load(j)
+
+files <- list.files("~/Dropbox/Civilingenior Kemi og Bioteknologi/3. Semester/R&D Project/GitHub/biogas/R", full.names = TRUE)
+for(i in files) source(i)
+
+files <- list.files("~/Dropbox/Civilingenior Kemi og Bioteknologi/3. Semester/R&D Project/GitHub/biogas/data", full.names = TRUE)
+for(j in files) load(j)
+
+# Required packages
+library(ggplot2)
+library(gridExtra)
+library(dplyr)
+
+# cumBgMan() test 'long' data structure
+cum.prodl.man <- cumBgMan(strawPressure, comp = strawComp, temp = 31,
+                     id.name = "bottle", time.name = "time", 
+                     dat.name = "pres", comp.name = "xCH4",
+                     temp.init = 21.55, pres.resid = "pres.resid", pres.init = 0.0,
+                     headspace = strawSetup, vol.hs.name = "headspace",
+                     pres.amb = 1.01, absolute = FALSE,
+                     extrap = TRUE, addt0 = TRUE)
+
+head(cum.prodl.man)
+
+# cumBg() same data
+cum.prodl <- cumBg(strawPressure, dat.type = "pres", comp = strawComp, temp = 31,
+      id.name = "bottle", time.name = "time",
+      dat.name = "pres", comp.name = "xCH4",
+      temp.init = 21.55, pres.resid = "pres.resid", pres.init = 0.0,
+      headspace = strawSetup, vol.hs.name = "headspace",
+      pres.amb = 1.01, absolute = FALSE,
+      extrap = TRUE,
+      addt0 = TRUE)
+
+head(cum.prodl)
+
+# Compare results from cumBgMan() and cumBg()
+all_equal(cum.prodl.man, cum.prodl, ignore_col_order = FALSE,
+          ignore_row_order = FALSE, convert = FALSE)
+
+# Plot results
+ggplot.1 <- ggplot(cum.prodl.man, aes(time, cvCH4, colour = bottle)) + 
+  geom_point() +
+  geom_line(aes(group = bottle)) +
+  labs(x = "Time [d]", y = "cvCH4  [mL]", colour = "Bottle no.")  + 
+  theme_bw() 
+
+ggplot.2 <- ggplot(cum.prodl, aes(time, cvCH4, colour = bottle)) + 
+  geom_point() +
+  geom_line(aes(group = bottle)) +
+  labs(x = "Time [d]", y = "cvCH4  [mL]", colour = "Bottle no.")  + 
+  theme_bw() 
+
+grid.arrange(ggplot.1, ggplot.2, ncol=1)
+
---
+++ tests/cumBgVol.test.R
@@ -0,0 +1,98 @@
+# Load data and functions
+#setwd() 
+
+#files <- list.files('biogas/R', full.names = TRUE)
+#for(i in files) source(i)
+
+#files <- list.files('biogas/data', full.names = TRUE)
+#for(j in files) load(j)
+
+files <- list.files("~/Dropbox/Civilingenior Kemi og Bioteknologi/3. Semester/R&D Project/GitHub/biogas/R", full.names = TRUE)
+for(i in files) source(i)
+
+files <- list.files("~/Dropbox/Civilingenior Kemi og Bioteknologi/3. Semester/R&D Project/GitHub/biogas/data", full.names = TRUE)
+for(j in files) load(j)
+
+# Required packages
+library(ggplot2)
+library(gridExtra)
+library(dplyr)
+
+
+# cumBgVol tests
+# longcombo data structure
+cpc <- cumBgVol(s3lcombo, temp = 25, pres = 1,
+             id.name = 'id', time.name = 'time.d',
+             data.struct = 'longcombo',
+             dat.name = 'vol.ml', comp.name = 'xCH4',
+             extrap = TRUE)
+
+# wide data structure
+cpw <- cumBgVol(s3volw, comp = s3compw, temp = 25, pres = 1,
+         time.name = 'time.d',
+         data.struct = 'wide',
+         dat.name = 'D', comp.name = 'D',
+         extrap = TRUE)
+
+# long data structure
+cpl <- cumBgVol(s3lcombo, comp = s3compl, temp = 25, pres = 1,
+             id.name = 'id', time.name = 'time.d',
+             dat.name = 'vol.ml', comp.name = 'xCH4',
+             extrap = TRUE)
+
+head(cpc)
+head(cpw)
+head(cpl)
+
+
+# Compare results from cumBgVol() and cumBg()
+# long data structure 
+# cumBgVol()
+cum.prodl <- cumBgVol(vol2, comp = comp2, temp = 20, pres = 1, 
+                     id.name = "bottle", time.name = "days", 
+                     dat.name = "meas.vol", comp.name = "CH4.conc", 
+                     extrap = TRUE)
+
+# cumBg()
+cum.prodln <- cumBg(vol2, comp = comp2, temp = 20, pres = 1, 
+                      id.name = "bottle", time.name = "days", 
+                      dat.name = "meas.vol", comp.name = "CH4.conc", 
+                      extrap = TRUE)
+
+head(cum.prodl)
+head(cum.prodln)
+
+all_equal(cum.prodln, cum.prodl, ignore_col_order = FALSE,
+          ignore_row_order = FALSE, convert = FALSE)
+
+# longcombo data structure
+# cumBgVol()
+cum.prodc <- cumBgVol(s3lcombo, temp = 25, pres = 1, 
+                     id.name = 'id', time.name = 'time.d', 
+                     data.struct = 'longcombo', dat.name = 'vol.ml', 
+                     comp.name = 'xCH4', extrap = TRUE)
+
+# cumBg()
+cum.prodcn <- cumBg(s3lcombo, temp = 25, pres = 1, 
+                      id.name = 'id', time.name = 'time.d', 
+                      data.struct = 'longcombo', dat.name = 'vol.ml', 
+                      comp.name = 'xCH4', extrap = TRUE)
+
+all_equal(cum.prodcn, cum.prodc, ignore_col_order = FALSE,
+          ignore_row_order = FALSE, convert = FALSE)
+
+# Plot longcombo results
+ggplot.1 <- ggplot(cum.prodc, aes(time.d, cvCH4, colour = id)) + 
+  geom_point() +
+  geom_line(aes(group = id)) +
+  labs(x = "Time [d]", y = "cvCH4  [mL]", colour = "ID")  + 
+  theme_bw() 
+
+ggplot.2 <- ggplot(cum.prodcn, aes(time.d, cvCH4, colour = id)) + 
+  geom_point() +
+  geom_line(aes(group = id)) +
+  labs(x = "Time [d]", y = "cvCH4  [mL]", colour = "ID")  + 
+  theme_bw() 
+
+grid.arrange(ggplot.1, ggplot.2, ncol=1)
+
